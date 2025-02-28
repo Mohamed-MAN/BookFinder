0532c7fbe892e19f3b8ab40088527597615c9f54
Mohamed-MAN <2023170532@cis.asu.edu.eg>
2025-02-28 08:19:08 +0200

    Update

--- config.template.js
+++ config.template.js
@@ -1,6 +1,4 @@
 // Rename this file to config.js and replace YOUR_API_KEY with your actual Google Books API key
-const CONFIG = {
+window.CONFIG = {
     GOOGLE_BOOKS_API_KEY: 'YOUR_API_KEY'
 };
-
-export default CONFIG;
